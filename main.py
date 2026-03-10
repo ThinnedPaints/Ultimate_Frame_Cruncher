acd4073955b0d42b6ee3158f2a64d6f9ece71cdf
ThinnedPaints <haj1337666@gmail.com>
2026-03-10 21:49:43 +0000

    It lives!
    Code works, needs lots of revisions, but fundamentally, this code does the job

--- main.py
+++ main.py
@@ -3,7 +3,9 @@ import time
 import datetime
 import moviepy
 import json
-from moviepy.editor import VideoFileClip
+import ast
+from tqdm import tqdm
+from moviepy.editor import VideoFileClip, concatenate_videoclips
 
 def get_time_in_min_and_second(seconds):
     #Get seconds as int, return minuites and seconds as str
@@ -11,26 +13,32 @@ def get_time_in_min_and_second(seconds):
 
 
 def process_video(timestamps):
-    with open("stamps.json", "r") as f:
-        timestamps = json.load(f)
+    print("Processing video")
+    for timestamp in timestamps:
+        timestamp[0] = timestamp[0]-90
+    main_video = VideoFileClip(video_path)
+    clips = [main_video.subclip(start, stop) for start, stop in timestamps]
+    final_clip = concatenate_videoclips(clips)
+    final_clip.write_videofile(output_video_path, codec="h264_nvenc", audio_codec="aac")
     
-    print(timestamps)
-    #main_video = VideoFileClip(video_path)
-    #clip = main_video.subclip()
-    #return
 
 def video_analysis(video_path, target_path):
-    #Analyse the video file and create an array of tuples containing timestamps
     start = time.time()
 
     video = cv2.VideoCapture(video_path)
     template = cv2.imread(target_path, 0)
+
     frame_count = 0
     frames_analysed = 0
+    total_frames = video.get(cv2.CAP_PROP_FRAME_COUNT)
 
     timestamps = []
     detecting = False
-    while True:
+    start_time = None
+
+    threshold = 0.95
+
+    for i in tqdm(range(int(total_frames))):
         ret, frame = video.read()
         if not ret:
             break
@@ -39,52 +47,46 @@ def video_analysis(video_path, target_path):
 
         if frame_count % 30 == 0:
             frames_analysed += 1
+
             video_time_in_seconds = frame_count / 30
-            video_time_true = get_time_in_min_and_second(video_time_in_seconds)
             gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
             result = cv2.matchTemplate(gray, template, cv2.TM_CCOEFF_NORMED)
-
-            threshold = 0.95
             max_val = result.max()
+
             if max_val >= threshold:
-                if detecting == False:
-                    print("Target detected at :", video_time_true)
-                    # print("match score:", max_val)
+                if not detecting:
                     detecting = True
-                    timestamps.append(("start", video_time_in_seconds))
+                    start_time = video_time_in_seconds
+
             else:
-                if detecting == True:
-                    print("Target gone at ", video_time_true)
+                if detecting:
                     detecting = False
-                    timestamps.append(("stop", video_time_in_seconds))
+                    timestamps.append([start_time, video_time_in_seconds])
 
     video.release()
 
     end = time.time()
     length = end - start
     total_time = get_time_in_min_and_second(length)
+
     print(total_time + " time taken")
     print(frames_analysed, " frames analysed")
 
-    json_timestamps = json.dumps(timestamps)
-
-    with open("stamps.json", "w") as f:
-        json.dump(json_timestamps, f)
+    for segment in timestamps:
+        print(segment)
 
     return timestamps
 
 media_path = "media/"
 video_path = media_path + "umar_bautista.mp4"
+output_video_path = media_path + "output.mp4"
 target_path = media_path + "target.png"
 
-no_Good_Name = False
+no_Good_Name = True
 if no_Good_Name == True:
     timestamps = video_analysis(video_path=video_path, target_path=target_path)
 
-timestamps = []
-
-
-process_video(timestamps=timestamps)
+process_video(timestamps)
 
 
